35a70bbeb26d20cc0a4fa1987b48d534e934bfe6
minatisleeping <minatt2002@gmail.com>
2023-05-08 12:08:52 +0700

    refactor the main code and green 4ever always

--- src/com/minat/mathutil/core/MathUtility.java
+++ src/com/minat/mathutil/core/MathUtility.java
@@ -28,7 +28,7 @@ public class MathUtility {
     public static long getFactorial(int n) {
         
         if (n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..20");
+            throw new IllegalArgumentException("Invalid argument. N must be between {0, 20}");
         
         if (n == 0 || n == 1)
             return 1; // end immediately if receive special input
